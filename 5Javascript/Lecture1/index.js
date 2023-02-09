270808c73a3e0f4de29e323cf64641cec73b7ffd
NileshAithani <Nileshaithani007@gmail.com>
2023-02-09 23:38:22 +0530

    add a discord project

---
+++ 5Javascript/Lecture1/index.js
@@ -0,0 +1,48 @@
+// console.log('Hello Everyone v5');
+
+
+
+// //question 1
+// // Create a variable og type string and try to add a number to it.
+
+// let a= 'Nilesh';
+// console.log(a+3);
+
+// //Question 2
+// //Type of use 
+// console.log(typeof(a+3));
+
+
+// //Question 3
+// // Creating a constant object and can we hold a number in it later
+
+// const object1 = {
+//     name:"Nilesh",
+//     age:22,
+// };
+
+// //  object1 =1;
+// //No we cant hold a number later in it
+
+// //Question4 - Can we a new Key in a object
+
+// object1['enroll']=1234;
+// object1['age']=12;
+
+// console.log(object1);
+
+
+
+
+// ..........Logical Operator..........
+
+let age =10;
+if (age>10 && age<20){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+
+
+
