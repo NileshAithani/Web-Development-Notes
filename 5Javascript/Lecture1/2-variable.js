da0706d0f5aa420d87d8c8164a405d298baf5b07
NileshAithani <Nileshaithani007@gmail.com>
2023-02-10 06:27:41 +0530

    add basic js things like variable

---
+++ 5Javascript/Lecture1/2-variable.js
@@ -0,0 +1,32 @@
+//three ways to create a variable
+//1-let  2-count  3-var
+
+
+//1
+
+console.log("var");
+var a=10;
+console.log(a);
+
+var a ='Nilesh';
+console.log(a);
+
+a=20;
+console.log(a);
+
+{
+    var a = 30;
+
+}
+console.log(a);
+
+
+//2 Let
+
+console.log("Let");
+let b = 10;
+console.log(b);
+
+//let b ='Nilesh' --- Shows error 
+b=40;
+console.log(b);  
