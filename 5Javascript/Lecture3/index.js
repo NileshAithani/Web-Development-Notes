ea822ace8b5573180a3b414f1db9fbce1086eb59
NileshAithani <Nileshaithani007@gmail.com>
2023-02-18 12:32:35 +0530

    about in-build object and array

---
+++ 5Javascript/Lecture3/index.js
@@ -0,0 +1,28 @@
+console.log('Hello Everyone');
+
+
+let firstname='Nilesh';
+
+
+
+
+//--------------Templet Literal-----------------
+
+let message = 'This is \nmy first \'message\'';
+//back tick is templet litral
+
+let message1 = 
+`this is
+my first 
+message`
+
+console.log(message);
+console.log(message1);
+
+
+
+//---------------Date & Time-------------------------
+let date = new Date();
+
+let date1 = new Date(2000, 1, 27, 2);
+console.log(date1);
